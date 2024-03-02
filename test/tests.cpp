f7d34056c20eaa25744665f7db0e4374a0a5942c
Daria Sokolova <dasa.vzn@mail.ru>
2024-03-02 22:06:54 +0300

    done tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,3 +4,17 @@
 #include "alg.h"
 
 
+TEST(CheckPrimeTest, TestPrimeNumber) {
+    EXPECT_TRUE(checkPrime(3));
+    EXPECT_TRUE(checkPrime(5));
+    EXPECT_TRUE(checkPrime(7));
+    EXPECT_TRUE(checkPrime(11));
+    EXPECT_TRUE(checkPrime(13));
+}
+TEST(CheckPrimeTest, TestCompositeNumber) {
+    EXPECT_FALSE(checkPrime(4));
+    EXPECT_FALSE(checkPrime(6));
+    EXPECT_FALSE(checkPrime(8));
+    EXPECT_FALSE(checkPrime(10));
+    EXPECT_FALSE(checkPrime(12));
+}
